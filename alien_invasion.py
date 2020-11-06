3ac95235800ae8acc0fddb7e1aa93af06017f9dd
William Kang <willkang7@gmail.com>
2020-11-05 23:52:08 -0500

    Created settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -8,12 +9,11 @@ class AlienInvasion:
 	def __init__(self):
 		"""Initialize the game, and create game resources."""
 		pygame.init()
+		self.settings = Settings()
 
-		self.screen = pygame.display.set_mode((600, 400))
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption('Alien Invasion')
-
-		# Set the background color.
-		self.bg_color = (230, 230, 230)
 
 	def run_game(self):
 		"""Starts the main loop for the game."""
@@ -24,10 +24,11 @@ class AlienInvasion:
 					sys.exit()
 
 			# Redraw the screen during each pass through the loop.
-			self.screen.fill(self.bg_color)
+			self.screen.fill(self.settings.bg_color)
 
 			# Make the most recently drawn screen visible.
 			pygame.display.flip()
+
 
 if __name__ == '__main__':
 	# Make a game instance, and run the game.
