768b1f8220e427ddb8421936874ebda8a9a6e4f4
William Kang <willkang7@gmail.com>
2020-11-07 10:48:56 -0500

    Updated screen size.

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
 	def __init__(self):
 		"""Initialize the game's settings."""
 		# Screen settings
-		self.screen_width = 600
-		self.screen_height = 400
+		self.screen_width = 900
+		self.screen_height = 600
 		self.bg_color = (160,172,254)
