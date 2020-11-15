dcee57251a88285946435e71f31d7f00f59f65c3
willkang7 <willkang7@gmail.com>
2020-11-15 15:42:15 -0500

    Added mario facing left mechanic.

--- ship.py
+++ ship.py
@@ -29,8 +29,10 @@ class Ship:
 		# Update the ship's x value, not the rect.
 		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.x += self.settings.ship_speed
+			self.image = pygame.image.load('images/mario.bmp')
 		if self.moving_left and self.rect.left > 0:
 			self.x -= self.settings.ship_speed
+			self.image = pygame.image.load('images/mario_left.bmp')
 
 		# Update rect object from self.x.
 		self.rect.x = self.x
