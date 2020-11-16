98ff6a822df77fcac3b7353c5beb502db7f55945
willkang7 <willkang7@gmail.com>
2020-11-15 22:34:13 -0500

    Added movement to aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
 			self._check_events()
 			self.ship.update()
 			self._update_bullets()
+			self._update_aliens()
 			self._update_screen()
 
 	def _check_events(self):
@@ -72,6 +73,14 @@ class AlienInvasion:
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
 
+	def _update_aliens(self):
+		"""
+		Check if the fleet is at an edge,
+		  then update the positions of all aliens in the fleet.
+		"""
+		self._check_fleet_edges()
+		self.aliens.update()
+
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
 		# Create an alien and find the number of aliens in a row.
@@ -100,6 +109,19 @@ class AlienInvasion:
 		alien.rect.x = alien.x
 		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
 		self.aliens.add(alien)
+
+	def _check_fleet_edges(self):
+		"""Respond appropriately if any aliens have reached an edge."""
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _change_fleet_direction(self):
+		"""Drop the entire fleet and change the fleet's direction."""
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction *= -1
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to the new screen."""
