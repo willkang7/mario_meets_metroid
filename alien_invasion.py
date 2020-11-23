dc3463175a5ca260c4da2a6eed0508ad50b5d75b
willkang7 <willkang7@gmail.com>
2020-11-22 22:39:33 -0500

    Added play button.

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from game_stats import GameStats
 from mario import Mario
 from bullet import Bullet
 from alien import Alien
+from button import Button
 
 
 class AlienInvasion:
@@ -31,6 +32,9 @@ class AlienInvasion:
 
 		self._create_fleet()
 
+		# Make the play button.
+		self.play_button = Button(self, 'Play')
+
 	def run_game(self):
 		"""Start the main loop for the game."""
 		while True:
@@ -52,6 +56,28 @@ class AlienInvasion:
 				self._check_keydown_events(event)
 			elif event.type == pygame.KEYUP:
 				self._check_keyup_events(event)
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				mouse_pos = pygame.mouse.get_pos()
+				self._check_play_button(mouse_pos)
+
+	def _check_play_button(self, mouse_pos):
+		"""Start a new game when the player clicks Play."""
+		button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+		if button_clicked and not self.stats.game_active:
+			# Reset the game statistics.
+			self.stats.reset_stats()
+			self.stats.game_active = True
+
+			# Get rid of any remaining aliens and bullets.
+			self.aliens.empty()
+			self.bullets.empty()
+
+			# Create a new fleet and center mario.
+			self._create_fleet()
+			self.mario.center_mario()
+
+			# Hide the mouse cursor.
+			pygame.mouse.set_visible(False)
 
 	def _check_keydown_events(self, event):
 		"""Respond to keypresses."""
@@ -136,6 +162,7 @@ class AlienInvasion:
 			sleep(0.5)
 		else:
 			self.stats.game_active = False
+			pygame.mouse.set_visible(True)
 
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
@@ -186,6 +213,10 @@ class AlienInvasion:
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
 		self.aliens.draw(self.screen)
+
+		# Draw the play button if the game is inactive.
+		if not self.stats.game_active:
+			self.play_button.draw_button()
 
 		pygame.display.flip()
 
