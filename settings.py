9819243c1257740d81fc17b8c55323866314ad2e
willkang7 <willkang7@gmail.com>
2020-11-29 21:55:17 -0500

    Added increasing difficulty.

--- settings.py
+++ settings.py
@@ -2,25 +2,40 @@ class Settings:
 	"""A class to store all settings for Alien Invasion."""
 
 	def __init__(self):
-		"""Initialize the game's settings."""
+		"""Initialize the game's static settings."""
 		# Screen settings
 		self.screen_width = 900
 		self.screen_height = 600
 		self.bg_color = (160,172,254)
 
 		# Mario settings
-		self.mario_speed = 0.5
 		self.mario_limit = 3
 
 		# Bullet settings
-		self.bullet_speed = 0.4
 		self.bullet_width = 15
 		self.bullet_height = 15
 		self.bullet_color = (252,85,18)
 		self.bullets_allowed = 3
 
 		# Alien settings
-		self.alien_speed = 0.2
 		self.fleet_drop_speed = 10
+
+		# How quickly the game speeds up
+		self.speedup_scale = 1.1
+
+		self.initialize_dynamic_settings()
+
+	def initialize_dynamic_settings(self):
+		"""Initialize settings that change throughout the game."""
+		self.mario_speed = 0.5
+		self.bullet_speed = 1.0
+		self.alien_speed = 0.33
+
 		# fleet_direction of 1 represents right; -1 represents left.
 		self.fleet_direction = 1
+
+	def increase_speed(self):
+		"""Increase speed settings."""
+		self.mario_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.alien_speed *= self.speedup_scale
